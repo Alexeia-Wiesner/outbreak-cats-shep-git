ac44f332d4973089db1995dfb45aa444c3cdba94
Amanda <amanda@catsandshepherd.com>
2017-03-17 09:54:47 +0200

    no message

--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,6 @@ export default
 {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost/outbreak-d',
+  database: 'mongodb://arcane-headland-17452.herokuapp.com/outbreak',
   mail_key: 'live_15bbf4e682d343e0254630eb1a551848b296fe56'
 }
